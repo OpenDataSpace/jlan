3e10508696fdf37b3c6c11c3ca156adcc5a8e0a2
Till Lorentzen <till.lorentzen@graudata.com>
2015-10-14 00:43:00 +0200

    Migrated to slf4j

--- alfresco-jlan/src/main/java/org/alfresco/jlan/oncrpc/RpcRequestThreadPool.java
+++ alfresco-jlan/src/main/java/org/alfresco/jlan/oncrpc/RpcRequestThreadPool.java
@@ -19,229 +19,201 @@
 
 package org.alfresco.jlan.oncrpc;
 
-import org.alfresco.jlan.debug.Debug;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * ONC/RPC Request Thread Pool Class
  *
- * <p>Processes RPC requests using a pool of worker threads.
+ * <p>
+ * Processes RPC requests using a pool of worker threads.
  *
  * @author gkspencer
  */
 public class RpcRequestThreadPool {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcRequestThreadPool.class);
+    // Default/minimum/maximum number of worker threads to use
+    public static final int DefaultWorkerThreads = 8;
+    public static final int MinimumWorkerThreads = 4;
+    public static final int MaximumWorkerThreads = 50;
 
-	//	Default/minimum/maximum number of worker threads to use
+    // Queue of RPC requests
+    private final RpcRequestQueue m_queue;
 
-	public static final int DefaultWorkerThreads			= 8;
-	public static final int MinimumWorkerThreads			= 4;
-	public static final int MaximumWorkerThreads			= 50;
+    // Worker threads
+    private final ThreadWorker[] m_workers;
 
-	//	Queue of RPC requests
+    // RPC dispatcher
+    private final RpcProcessor m_rpcProcessor;
 
-	private RpcRequestQueue m_queue;
+    // Debug enable flag
+    private static boolean m_debug = true;
 
-	//	Worker threads
+    /**
+     * Thread Worker Inner Class
+     */
+    protected class ThreadWorker implements Runnable {
+        // Worker thread
+        private final Thread mi_thread;
 
-	private ThreadWorker[] m_workers;
+        // Shutdown flag
+        private boolean mi_shutdown = false;
 
-	//	RPC dispatcher
+        /**
+         * Class constructor
+         *
+         * @param name
+         *            String
+         * @param id
+         *            int
+         */
+        public ThreadWorker(final String name, final int id) {
+            // Create the worker thread
+            mi_thread = new Thread(this);
+            mi_thread.setName(name);
+            mi_thread.setDaemon(true);
+            mi_thread.start();
+        }
 
-	private RpcProcessor m_rpcProcessor;
+        /**
+         * Request the worker thread to shutdown
+         */
+        public final void shutdownRequest() {
+            mi_shutdown = true;
+            try {
+                mi_thread.interrupt();
+            } catch (final Exception ex) {
+            }
+        }
 
-	//	Debug enable flag
+        /**
+         * Run the thread
+         */
+        @Override
+        public void run() {
+            // Loop until shutdown
+            RpcPacket rpc = null;
+            RpcPacket response = null;
+            while (mi_shutdown == false) {
+                try {
+                    // Wait for an RPC request to be queued
+                    rpc = m_queue.removeRequest();
+                } catch (final InterruptedException ex) {
+                    // Check for shutdown
+                    if (mi_shutdown == true) {
+                        break;
+                    }
+                }
 
-	private static boolean m_debug = true;
+                // If the request is valid process it
+                if (rpc != null) {
+                    try {
+                        // Process the request
+                        response = m_rpcProcessor.processRpc(rpc);
+                        if (response != null) {
+                            response.getPacketHandler().sendRpcResponse(response);
+                        }
+                    } catch (final Throwable ex) {
+                        // Do not display errors if shutting down
+                        if (mi_shutdown == false) {
+                            LOGGER.error("Worker " + Thread.currentThread().getName() + ":", ex);
+                        }
+                    } finally {
+                        // Release the RPC packet(s) back to the packet pool
+                        if (rpc.getClientProtocol() == Rpc.TCP && rpc.isAllocatedFromPool()) {
+                            rpc.getOwnerPacketPool().releasePacket(rpc);
+                        }
 
-	/**
-	 * Thread Worker Inner Class
-	 */
-	protected class ThreadWorker implements Runnable {
+                        if (response != null && response.getClientProtocol() == Rpc.TCP && response.getBuffer() != rpc.getBuffer()
+                                && response.isAllocatedFromPool()) {
+                            response.getOwnerPacketPool().releasePacket(response);
+                        }
 
-		//	Worker thread
+                    }
+                }
+            }
+        }
+    };
 
-		private Thread mi_thread;
+    /**
+     * Class constructor
+     *
+     * @param threadName
+     *            String
+     * @param rpcServer
+     *            RpcProcessor
+     */
+    public RpcRequestThreadPool(final String threadName, final RpcProcessor rpcServer) {
+        this(threadName, DefaultWorkerThreads, rpcServer);
+    }
 
-		//	Worker unique id
+    /**
+     * Class constructor
+     *
+     * @param threadName
+     *            String
+     * @param poolSize
+     *            int
+     * @param rpcServer
+     *            RpcProcessor
+     */
+    public RpcRequestThreadPool(final String threadName, int poolSize, final RpcProcessor rpcServer) {
+        // Save the RPC handler
+        m_rpcProcessor = rpcServer;
 
-		private int mi_id;
+        // Create the request queue
+        m_queue = new RpcRequestQueue();
 
-		//	Shutdown flag
+        // Check that we have at least minimum worker threads
+        if (poolSize < MinimumWorkerThreads) {
+            poolSize = MinimumWorkerThreads;
+        }
 
-		private boolean mi_shutdown = false;
+        // Create the worker threads
+        m_workers = new ThreadWorker[poolSize];
 
-		/**
-		 * Class constructor
-		 *
-		 * @param name String
-		 * @param id int
-		 */
-		public ThreadWorker(String name, int id) {
+        for (int i = 0; i < m_workers.length; i++) {
+            m_workers[i] = new ThreadWorker(threadName + (i + 1), i);
+        }
+    }
 
-			//	Save the thread id
+    /**
+     * Check if debug output is enabled
+     *
+     * @return boolean
+     */
+    public final static boolean hasDebug() {
+        return m_debug;
+    }
 
-			mi_id     = id;
+    /**
+     * Return the number of requests in the queue
+     *
+     * @return int
+     */
+    public final int getNumberOfRequests() {
+        return m_queue.numberOfRequests();
+    }
 
-			//	Create the worker thread
+    /**
+     * Queue an RPC request to the thread pool for processing
+     *
+     * @param pkt
+     *            RpcPacket
+     */
+    public final void queueRpcRequest(final RpcPacket pkt) {
+        m_queue.addRequest(pkt);
+    }
 
-			mi_thread = new Thread(this);
-			mi_thread.setName(name);
-			mi_thread.setDaemon(true);
-			mi_thread.start();
-		}
-
-		/**
-		 * Request the worker thread to shutdown
-		 */
-		public final void shutdownRequest() {
-			mi_shutdown = true;
-			try {
-				mi_thread.interrupt();
-			}
-			catch (Exception ex) {
-			}
-		}
-
-		/**
-		 * Run the thread
-		 */
-		public void run() {
-
-			//	Loop until shutdown
-
-			RpcPacket rpc = null;
-			RpcPacket response = null;
-
-			while ( mi_shutdown == false) {
-
-				try {
-
-					//	Wait for an RPC request to be queued
-
-					rpc = m_queue.removeRequest();
-				}
-				catch (InterruptedException ex) {
-
-					//	Check for shutdown
-
-					if ( mi_shutdown == true)
-						break;
-				}
-
-				//	If the request is valid process it
-
-				if ( rpc != null) {
-
-					try {
-
-						//	Process the request
-
-					  response = m_rpcProcessor.processRpc(rpc);
-					  if ( response != null)
-					    response.getPacketHandler().sendRpcResponse(response);
-					}
-					catch (Throwable ex) {
-
-					  //	Do not display errors if shutting down
-
-					  if ( mi_shutdown == false) {
-						  Debug.println("Worker " + Thread.currentThread().getName() + ":");
-						  Debug.println(ex);
-					  }
-					}
-					finally {
-
-					  //	Release the RPC packet(s) back to the packet pool
-
-					  if ( rpc.getClientProtocol() == Rpc.TCP && rpc.isAllocatedFromPool())
-					    rpc.getOwnerPacketPool().releasePacket(rpc);
-
-					  if ( response != null && response.getClientProtocol() == Rpc.TCP &&
-					       response.getBuffer() != rpc.getBuffer() && response.isAllocatedFromPool())
-					    response.getOwnerPacketPool().releasePacket(response);
-
-					}
-				}
-			}
-		}
-	};
-
-	/**
-	 * Class constructor
-	 *
-	 * @param threadName String
-	 * @param rpcServer RpcProcessor
-	 */
-	public RpcRequestThreadPool(String threadName, RpcProcessor rpcServer) {
-		this(threadName, DefaultWorkerThreads, rpcServer);
-	}
-
-	/**
-	 * Class constructor
-	 *
-	 * @param threadName String
-	 * @param poolSize int
-	 * @param rpcServer RpcProcessor
-	 */
-	public RpcRequestThreadPool(String threadName, int poolSize, RpcProcessor rpcServer) {
-
-		//	Save the RPC handler
-
-		m_rpcProcessor  = rpcServer;
-
-		//	Create the request queue
-
-		m_queue = new RpcRequestQueue();
-
-		//	Check that we have at least minimum worker threads
-
-		if ( poolSize < MinimumWorkerThreads)
-			poolSize = MinimumWorkerThreads;
-
-		//	Create the worker threads
-
-		m_workers = new ThreadWorker[poolSize];
-
-		for ( int i = 0; i < m_workers.length; i++)
-			m_workers[i] = new ThreadWorker(threadName + ( i+1), i);
-	}
-
-	/**
-	 * Check if debug output is enabled
-	 *
-	 * @return boolean
-	 */
-	public final static boolean hasDebug() {
-		return m_debug;
-	}
-
-	/**
-	 * Return the number of requests in the queue
-	 *
-	 * @return int
-	 */
-	public final int getNumberOfRequests() {
-		return m_queue.numberOfRequests();
-	}
-
-	/**
-	 * Queue an RPC request to the thread pool for processing
-	 *
-	 * @param pkt RpcPacket
-	 */
-	public final void queueRpcRequest(RpcPacket pkt) {
-	  m_queue.addRequest(pkt);
-	}
-
-	/**
-	 * Shutdown the thread pool and release all resources
-	 */
-	public void shutdownThreadPool() {
-
-		//	Shutdown the worker threads
-
-		if ( m_workers != null) {
-			for ( int i = 0; i < m_workers.length; i++)
-				m_workers[i].shutdownRequest();
-		}
-	}
+    /**
+     * Shutdown the thread pool and release all resources
+     */
+    public void shutdownThreadPool() {
+        // Shutdown the worker threads
+        if (m_workers != null) {
+            for (final ThreadWorker m_worker : m_workers) {
+                m_worker.shutdownRequest();
+            }
+        }
+    }
 }
